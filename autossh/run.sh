db009633e1e686a306bfc66d8d1ef5d596320cdb
toddw65 <5347861+toddw65@users.noreply.github.com>
2017-10-28 18:16:53 -0400

    Update run.sh

--- autossh/run.sh
+++ autossh/run.sh
@@ -27,7 +27,8 @@ fi
 echo "[INFO] public key is:"
 cat "${KEY_PATH}/autossh_rsa_key.pub"
 
-command_args="-M ${MONITOR_PORT} -N -q -o ServerAliveInterval=20 -o ServerAliveCountMax=3 ${USERNAME}@${HOSTNAME} -i ${KEY_PATH}/autossh_rsa_key"
+#command_args="-M ${MONITOR_PORT} -N -q -o ServerAliveInterval=20 -o ServerAliveCountMax=3 ${USERNAME}@${HOSTNAME} -i ${KEY_PATH}/autossh_rsa_key"
+command_args="-fN -o ServerAliveInterval=20 -o ServerAliveCountMax=3 ${USERNAME}@${HOSTNAME} -i ${KEY_PATH}/autossh_rsa_key"
 
 if [ ! -z "$REMOTE_FORWARDING" ]; then
   while read -r line; do
